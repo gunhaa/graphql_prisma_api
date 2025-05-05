bcb2b71e8c2b6a75bf9f82523189bb185d889510
gunha <wh8299@gmail.com>
2025-05-05 14:05:09 +0900

    feat : getMember, setMember resolver와 기능 추가

---
+++ src/graphql/entities/dtos/createMemberDto.ts
@@ -0,0 +1,35 @@
+import {
+    IsEmail,
+    IsNotEmpty,
+    IsString,
+    Matches,
+    MaxLength,
+    MinLength,
+} from "class-validator";
+
+export class CreateMemberDto {
+
+    @IsEmail()
+    @IsNotEmpty()
+    email!: string;
+
+    @IsString()
+    name: string | null;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(8)
+    @MaxLength(20)
+    password!: string;
+
+    @IsString()
+    @IsNotEmpty()
+    address!: string;
+
+    constructor(email: string, name: string | null, password: string, address: string){
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.address = address;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import {startApolloServer} from "./server"
+import { startApolloServer } from "./server"
 import { gql } from "graphql-tag";
 import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge';
+import prismaClient from "../prisma/prismaClient";
+import { Member } from "@prisma/client";
+import { CreateMemberDto } from "./graphql/entities/dtos/createMemberDto";
+import { validateOrReject } from "class-validator";
+
 
 const typeDefsRaw = gql`
 
@@ -24,13 +29,52 @@ const typeDefsRaw = gql`
   }
   
   type Query {
-    members: [Member]
+    getMembers: [Member]
   }
+
+  type Mutation {
+    setMember(input: MemberCreateInput!): Member
+  }
+
+  input MemberCreateInput {
+    email: String!
+    name: String
+    password: String!
+    address: String
+  }
+
 `
 
 const resolversRaw = {
-  Query : {
-    members : () => 
+  Query: {
+    getMembers : (_: void , args: any) => prismaClient.member.findMany()
+  },
+
+  Mutation: {
+    setMember: async ( 
+      _parent: void, 
+      args: { input: CreateMemberDto }
+    ): Promise<Member> => {
+      const createMemberDto = new CreateMemberDto(
+        args.input.email,
+        args.input.name,
+        args.input.password,
+        args.input.address
+      );
+
+      await validateOrReject(createMemberDto);
+
+      // 멤버 생성 값 반환
+      return await prismaClient.member.create({
+        data: {
+          email: createMemberDto.email,
+          name: createMemberDto.name,
+          password: createMemberDto.password,
+          address: createMemberDto.address,
+          createdAt: new Date(),
+        }
+      });
+    }
   }
 }
 
